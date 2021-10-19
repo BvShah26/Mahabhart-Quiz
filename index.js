1a9bcb5465c13a57ecb9d2cf16114cd719f09345
BvShah26 <92227795+BvShah26@users.noreply.github.com>
2021-10-19 23:30:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+var name = "";
+var score = 0;
+var i = 0;
+var rightAns=0;
+var wrongAns=0;
+
+//Getting Questions object from another file
+const questions=require("./questions");
+
+//Starting Game
+const start = () => {
+    rl.question("\nPlease Enter Your Name :: ", (ans) => {
+        name = ans;
+        console.log(`\nWelcome ${name} In Game :: Do You Know MAHABHARAT`);
+        console.log("\n=: Rules :=\n");
+        console.log("There will be 5 questions in the game");
+        console.log("For correct answer +5 and for wrong answer -2");
+        quiz();
+    });
+}
+
+const quiz = () => {
+    if (i >= questions.length) {
+        console.log("\n\n =: RESULT :=");
+        console.log(`\nTotal Correct Answers :: ${rightAns}\nTotal Wrong Answers :: ${wrongAns}`);
+        console.log(`\n${name} Your Final Score is :: ${score}`);
+        rl.close();
+        return;
+    }
+    console.log(`\nQuestion ${i + 1} :: ${questions[i].que}`);
+    console.log();
+    var j = 1;
+    questions[i].opt.forEach((item) => {
+        console.log(`(${j}).  ${item}`);
+        j += 1;
+    });
+    askAnswer();
+};
+
+const askAnswer = () => {
+    rl.question("\nEnter Your Option Number :: ", (res) => {
+       
+        const index = questions[i].opt.indexOf(questions[i].ans);
+        if (index == (res - 1)) {
+            console.log("\nYour Answer is Correct...!\nYou Got 5 Score For this Question");
+            score += 5;
+            rightAns+=1;
+        }
+        else {
+            console.log(`\nOpps Wrong Answer..!\nCorrect Answer is :: ${questions[i].ans}\nYou Got -2 Score For this question`);
+            score -= 2;
+            wrongAns+=1;
+        }
+
+        i++;
+        quiz();
+    });
+};
+
+start();
---
+++ questions.js
@@ -0,0 +1,30 @@
+const questions=[
+    {
+       "que":"How many children did Dhritarashtra & Gandhari have ?",
+       "opt":["100","101","102","103"],
+       "ans":"101" 
+    },
+    {
+        "que":"How many children did Pandav have ?",
+        "opt":["7","3","5","101"],
+        "ans":"5" 
+     },
+     {
+        "que":"Why did Gandhari keep a blindfold ?",
+        "opt":["Because she didn’t want to see a world that her husband cannot see","Because she was cursed","Because she didn’t want to witness the war","Not any of them"],
+        "ans":"Because she didn’t want to see a world that her husband cannot see" 
+     },
+     {
+        "que":"What was Vasudeva's (Father of Lord Krishna) sister’s name? ?",
+        "opt":["Srutasava","Kunti","Devaki","Devsena"],
+        "ans":"Kunti" 
+     },
+     {
+        "que":"SUBHADRA Married to whom ?",
+        "opt":["Bheem","Yudhisthir","Duryodhan","Arjun"],
+        "ans":"Arjun" 
+     },
+     
+];
+
+module.exports=questions;
